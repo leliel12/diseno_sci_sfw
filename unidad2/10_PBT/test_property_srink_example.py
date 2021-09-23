b981dcd178fec183e44be67c715b54872e0bbf9f
JuanBC <jbc.develop@gmail.com>
2021-09-22 21:23:43 -0300

    PBT

---
+++ unidad2/10_PBT/test_property_srink_example.py
@@ -0,0 +1,14 @@
+def sum(num1, num2):
+    """It returns sum of two numbers"""
+    return num1 + num2
+
+
+from hypothesis import given, settings, Verbosity, example
+from hypothesis import strategies as st
+
+@settings(verbosity=Verbosity.verbose)
+@given(st.integers(), st.integers())
+def test_sum(num1, num2):
+    assert sum(num1, 0) == num1
+    assert sum(num1, num2) == sum(num2, num1)
+    assert num1 <= 30
