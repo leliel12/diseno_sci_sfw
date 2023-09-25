66715a9dace374fb08f049eccd2df7bd931c3de5
juanbc <jbcabral@unc.edu.ar>
2023-09-25 00:43:42 -0300

    unidad 3

---
+++ 01_paradigmas/prolog/db.pl
@@ -0,0 +1,17 @@
+man(adam).
+man(peter).
+man(paul).
+
+woman(marry).
+woman(eve).
+
+parent(adam, peter).
+parent(eve, peter).
+parent(adam, paul).
+parent(marry, paul).
+
+
+father(F, C) :- man(F), parent(F, C).
+mother(M, C) :- woman(M), parent(M, C).
+
+
